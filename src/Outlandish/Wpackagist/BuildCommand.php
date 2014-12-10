ebd6c1a2c6a30e4a918a6d39a21e714b93d93e58
Sébastien Lavoie <seb@wemakecustom.com>
2014-12-10 02:39:01 -0500

    split providers by smaller groups

--- src/Outlandish/Wpackagist/BuildCommand.php
+++ src/Outlandish/Wpackagist/BuildCommand.php
@@ -8,6 +8,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Helper\Helper;
+use Outlandish\Wpackagist\Package\AbstractPackage;
 
 class BuildCommand extends Command
 {
@@ -16,6 +17,36 @@ class BuildCommand extends Command
         $this
             ->setName('build')
             ->setDescription('Build package.json from DB');
+    }
+
+    /**
+     * Return a string to split packages in more-or-less even groups
+     * of their last modification. Minimizes groups modifications.
+     *
+     * @return string
+     */
+    protected function getComposerProviderGroup(AbstractPackage $package)
+    {
+        $date = $package->getLastCommited();
+
+        if ($date >= new \DateTime('monday last week')) {
+            return 'this-week';
+        } elseif ($date >= new \DateTime(date('Y') . '-01-01')) {
+            // split current by chunks of 3 months, current month included
+            // past chunks will never be update this year
+            $month = $date->format('n');
+            $month = ceil($month / 3) * 3;
+            $month = str_pad($month, 2, '0', STR_PAD_LEFT);
+
+            return $date->format('Y-') . $month;
+        } elseif ($date >= new \DateTime('2011-01-01')) {
+            // split by years, limit at 2011 so we never update 'old' again
+            return $date->format('Y');
+        } else {
+            // 2010 and older is about 2M, which is manageable
+            // Still some packages ? Probably junk/errors
+            return 'old';
+        }
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -52,7 +83,7 @@ class BuildCommand extends Command
                 $content = json_encode(array('packages' => array($packageName => $packageData)));
                 $sha256 = hash('sha256', $content);
                 file_put_contents("$basePath$packageName\$$sha256.json", $content);
-                $providers[$package->getComposerProviderGroup()][$packageName] = array(
+                $providers[$this->getComposerProviderGroup($package)][$packageName] = array(
                     'sha256' => $sha256
                 );
             }
