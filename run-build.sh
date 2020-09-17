d441df51fb14b4f0e9f289ab84336205b608069d
Noel Light-Hilary <noel@noellh.com>
2020-09-17 19:00:46 +0100

    Add `run-build.sh` script for smaller, more frequent updates

---
+++ run-build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+php bin/console build
--- run-cron.sh
+++ run-cron.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Example cron script. The actual cron script on wpackagist.org is more complex, and involves ensuring generated files have the correct permissions.
 php bin/console migrate
 php bin/console refresh
 php bin/console update
