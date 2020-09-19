c8bda4e40238487a29316956fac2416df585151b
Noel Light-Hilary <noel@noellh.com>
2020-09-19 09:31:46 +0100

    More durable permissions dance

--- run-build.sh
+++ run-build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # Ensure the default web server user `www-data` can also write info (e.g.
-# IP rate limiting data) to SQLite.
-chown -R "$(whoami)" "${PACKAGE_PATH}"
-chgrp -R www-data "${PACKAGE_PATH}"
-find "${PACKAGE_PATH}" -type d -exec chmod 775 {} +
-find "${PACKAGE_PATH}" -type f -exec chmod 664 {} +
+# IP rate limiting data) to SQLite. Permit failures because another process
+# could be performing operations including deletes in parallel.
+chown -R "$(whoami)" "${PACKAGE_PATH}" || true
+chgrp -R www-data "${PACKAGE_PATH}" || true
+find "${PACKAGE_PATH}" -type d -exec chmod 775 {} + || true
+find "${PACKAGE_PATH}" -type f -exec chmod 664 {} + || true
 
 php bin/console build
 
