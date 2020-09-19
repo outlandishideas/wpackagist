6a7b0deefb5a4afd1daa6efd9346b35f7e1780d6
Noel Light-Hilary <noel@noellh.com>
2020-09-19 09:05:39 +0100

    Remove sudo
    Not available in container + task runs as root anyway

--- run-build.sh
+++ run-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Ensure the default web server user `www-data` can also write info (e.g.
 # IP rate limiting data) to SQLite.
-sudo chown -R "$(whoami)" "${PACKAGE_PATH}"
+chown -R "$(whoami)" "${PACKAGE_PATH}"
 chgrp -R www-data "${PACKAGE_PATH}"
 find "${PACKAGE_PATH}" -type d -exec chmod 775 {} +
 find "${PACKAGE_PATH}" -type f -exec chmod 664 {} +
