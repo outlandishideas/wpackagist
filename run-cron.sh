0019e4e962ba4636e8e02a8e9057111d012bf52c
Noel Light-Hilary <noel@noellh.com>
2020-09-19 08:54:51 +0100

    Try a permission fix closer to legacy infra's

--- run-cron.sh
+++ run-cron.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+# Ensure the default web server user `www-data` can also write info (e.g.
+# IP rate limiting data) to SQLite.
+sudo chown -R "$(whoami)" "${PACKAGE_PATH}"
+chgrp -R www-data "${PACKAGE_PATH}"
+find "${PACKAGE_PATH}" -type d -exec chmod 775 {} +
+find "${PACKAGE_PATH}" -type f -exec chmod 664 {} +
+
 php bin/console migrate
 php bin/console refresh
 php bin/console update
-php bin/console build
-
-# Ensure the default web server user `www-data` can also write info (e.g.
-# IP rate limiting data) to SQLite.
-chmod 0666 "${PACKAGE_PATH}/packages.sqlite"
+php bin/console build --force
 
 echo "Done!"
