9c012c797d67cc6d51810f9f9cc32dd0877560d3
Noel Light-Hilary <noel@noellh.com>
2020-09-19 07:09:40 +0100

    Ensure the default web server user `www-data` can also write info (e.g. IP rate limiting data) to SQLite.
    As nothing else shares or can access the deployed filesystem,
    this should be reasonably safe here and avoids having to maintain
    non-standard users / groups for the two different runtime mdoes.

--- run-cron.sh
+++ run-cron.sh
@@ -4,4 +4,8 @@ php bin/console refresh
 php bin/console update
 php bin/console build
 
+# Ensure the default web server user `www-data` can also write info (e.g.
+# IP rate limiting data) to SQLite.
+chmod 0666 "${PACKAGE_PATH}/packages.sqlite"
+
 echo "Done!"
