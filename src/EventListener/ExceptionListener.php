4415cca7eac1c949ef5ad503bbce6dc9495b082a
Noel Light-Hilary <noel@noellh.com>
2020-09-25 16:41:25 +0100

    Log staging/production crash details

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -2,12 +2,20 @@
 
 namespace Outlandish\Wpackagist\EventListener;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 class ExceptionListener
 {
+    private LoggerInterface $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     public function onKernelException(ExceptionEvent $event)
     {
         // Let Symfony's default error tracing happen in dev.
@@ -18,6 +26,8 @@ class ExceptionListener
         $message = 'Something went wrong.';
         $exception = $event->getThrowable();
         $response = new Response();
+
+        $this->logger->critical(sprintf('%s – %s', get_class($exception), $exception->getMessage()));
 
         if ($exception instanceof HttpExceptionInterface) {
             $response->setStatusCode($exception->getStatusCode());
