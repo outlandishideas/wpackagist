c57ce97110e9bbb917cf8d80031af7234d6c727b
Noel Light-Hilary <noel@noellh.com>
2020-09-25 15:38:48 +0100

    Skip insanely slow file operations in tasks for now

--- run-cron.sh
+++ run-cron.sh
@@ -2,10 +2,13 @@
 # Ensure the default web server user `www-data` can also write info (e.g.
 # IP rate limiting data) to SQLite. Permit failures because another process
 # could be performing operations including deletes in parallel.
-chown -R "$(whoami)" "${PACKAGE_PATH}" || true
-chgrp -R www-data "${PACKAGE_PATH}" || true
-find "${PACKAGE_PATH}" -type d -exec chmod 775 {} + || true
-find "${PACKAGE_PATH}" -type f -exec chmod 664 {} + || true
+#
+# See if we can get away without this, now that EFS has been set up in each env.
+# TODO figure out a more durable solution.
+#chown -R "$(whoami)" "${PACKAGE_PATH}" || true
+#chgrp -R www-data "${PACKAGE_PATH}" || true
+#find "${PACKAGE_PATH}" -type d -exec chmod 775 {} + || true
+#find "${PACKAGE_PATH}" -type f -exec chmod 664 {} + || true
 
 echo "Starting refresh..."
 php bin/console refresh
