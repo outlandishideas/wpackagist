f9d9abbf40cd9a0fa8b39a88e3d3c76b648e6f38
Noel Light-Hilary <noel@noellh.com>
2021-02-06 09:27:04 +0000

    Extend 'not found' low severity log logic to bad methods; simplify checks

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -7,6 +7,8 @@ use Psr\Log\LogLevel;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ExceptionListener
 {
@@ -28,24 +30,24 @@ class ExceptionListener
         $exception = $event->getThrowable();
         $response = new Response();
 
-        $is404 = false;
         if ($exception instanceof HttpExceptionInterface) {
             $response->setStatusCode($exception->getStatusCode());
             $response->headers->replace($exception->getHeaders());
-
-            if ($exception->getStatusCode() === 404) {
-                $is404 = true;
-            }
         } else {
             $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
         }
 
+        $notFound = (
+            $exception instanceof NotFoundHttpException ||
+            $exception instanceof MethodNotAllowedHttpException
+        );
+
         $this->logger->log(
-            $is404 ? LogLevel::INFO : LogLevel::CRITICAL,
+            $notFound ? LogLevel::INFO : LogLevel::CRITICAL,
             sprintf('%s – %s', get_class($exception), $exception->getMessage())
         );
 
-        $response->setContent($is404 ? 'The requested page could not be found.' : 'Something went wrong.');
+        $response->setContent($notFound ? 'The requested page could not be found.' : 'Something went wrong.');
 
         $event->setResponse($response);
     }
